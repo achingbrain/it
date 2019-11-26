68b4037bd024f03078d59f34df8ba43c4e78ecf8
achingbrain <alex@achingbrain.net>
2019-11-26 11:37:16 +0000

    add default batch size

--- test.js
+++ test.js
@@ -9,3 +9,10 @@ test('Should batch up entries', async (t) => {
 
   t.deepEqual(res, [[0, 1], [2, 3], [4]])
 })
+
+test('Should batch up entries without batch size', async (t) => {
+  const values = [0, 1, 2, 3, 4]
+  const res = await all(batch(values))
+
+  t.deepEqual(res, [[0], [1], [2], [3], [4]])
+})
