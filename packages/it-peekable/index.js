97f3183bcf396e720eaa0265855aca4fa881234e
Irakli Gozalishvili <contact@gozala.io>
2020-08-06 14:38:14 -0700

    feat: add types to peekable

--- packages/it-peekable/index.js
+++ packages/it-peekable/index.js
@@ -1,10 +1,56 @@
 'use strict'
 
+/**
+ * @template T
+ * @typedef {Object} Peek
+ * @property {() => IteratorResult<T, void>} peek
+ */
+
+/**
+ * @template T
+ * @typedef {Object} AsyncPeek
+ * @property {() => Promise<IteratorResult<T, void>>} peek
+ */
+
+/**
+ * @template T
+ * @typedef {Object} Push
+ * @property {(value:T) => void} push
+ */
+
+/**
+ * @template T
+ * @typedef {Iterable<T> & Peek<T> & Push<T> & Iterator<T>} Peekable<T>
+ */
+
+/**
+ * @template T
+ * @typedef {AsyncIterable<T> & AsyncPeek<T> & Push<T> & AsyncIterator<T>} AsyncPeekable<T>
+ */
+
+/**
+ * @template {Iterable<any> | AsyncIterable<any>} I
+ * @param {I} iterable
+ * @returns {I extends Iterable<infer T>
+ *  ? Peekable<T>
+ *  : I extends AsyncIterable<infer T>
+ *  ? AsyncPeekable<T>
+ *  : never
+ * }
+ */
 function peekableIterator (iterable) {
-  const iterator = iterable[Symbol.asyncIterator] ? iterable[Symbol.asyncIterator]() : iterable[Symbol.iterator]()
+  // @ts-ignore
+  const [iterator, symbol] = iterable[Symbol.asyncIterator]
+    // @ts-ignore
+    ? [iterable[Symbol.asyncIterator](), Symbol.asyncIterator]
+    // @ts-ignore
+    : [iterable[Symbol.iterator](), Symbol.iterator]
+
+  /** @type {any[]} */
   const queue = []
 
-  const peekable = {
+  // @ts-ignore
+  return {
     peek: () => {
       return iterator.next()
     },
@@ -20,20 +66,11 @@ function peekableIterator (iterable) {
       }
 
       return iterator.next()
+    },
+    [symbol] () {
+      return this
     }
   }
-
-  if (iterable[Symbol.asyncIterator]) {
-    peekable[Symbol.asyncIterator] = () => {
-      return peekable
-    }
-  } else {
-    peekable[Symbol.iterator] = () => {
-      return peekable
-    }
-  }
-
-  return peekable
 }
 
 module.exports = peekableIterator
