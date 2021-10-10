5f67f71c6928a20fc666ee04d19b220b9f0e0281
achingbrain <alex@achingbrain.net>
2021-10-10 12:08:37 +0100

    feat: add ordered option
    Adds an option to return ordered or unordered results
    
    ```js
    for await (const foo of parallel(source, {
        ordered: true // default: false
        concurrency: 5 // default: Infinity
      })) {
      //...
    }
    ```
    
    BREAKING CHANGE: results are out-of-order by default and default concurrency has increased from 1 to Inifnity

--- packages/it-parallel/test.js
+++ packages/it-parallel/test.js
@@ -13,17 +13,36 @@ const createFn = (ms, result) => {
   }
 }
 
-test('Should execute', async (t) => {
+test('Should execute and return ordered results', async (t) => {
   const input = [
     createFn(1000, 1),
     createFn(2000, 2),
     createFn(100, 3),
-    createFn(100, 4)
+    createFn(500, 4)
   ]
 
-  const res = await all(parallel(input, 3))
+  const res = await all(parallel(input, {
+    concurrency: 3,
+    ordered: true
+  }))
 
   t.deepEqual(res, [1, 2, 3, 4])
+})
+
+test('Should execute and return unordered results', async (t) => {
+  const input = [
+    createFn(1000, 1),
+    createFn(2000, 2),
+    createFn(100, 3),
+    createFn(500, 4)
+  ]
+
+  const res = await all(parallel(input, {
+    concurrency: 3,
+    ordered: false
+  }))
+
+  t.deepEqual(res, [3, 4, 1, 2])
 })
 
 test('Should not exceed concurrency limit', async (t) => {
@@ -55,14 +74,14 @@ test('Should not exceed concurrency limit', async (t) => {
     createFn(10, 5)
   ]
 
-  const res = await all(parallel(input, concurrency))
+  const res = await all(parallel(input, { concurrency, ordered: true }))
 
   t.deepEqual(res, [1, 2, 3, 4, 5])
 
   t.is(runningMax, concurrency)
 })
 
-test('Should propagate errors', async (t) => {
+test('Should propagate task errors', async (t) => {
   const error = new Error('wat')
 
   const input = [
@@ -76,19 +95,92 @@ test('Should propagate errors', async (t) => {
   ]
 
   try {
-    await all(parallel(input, 2))
+    await all(parallel(input, { concurrency: 2 }))
   } catch (err) {
     t.is(err, error)
   }
 })
 
-test('Should work without size parameter', async (t) => {
+test('Should propagate source errors', async (t) => {
+  const error = new Error('Urk!')
+
+  async function * source () {
+    yield async () => 'foo'
+
+    throw error
+  }
+
+  try {
+    await all(parallel(source(), { concurrency: 2 }))
+  } catch (err) {
+    t.is(err, error)
+  }
+})
+
+test('Should allow source to finish if task errors', async (t) => {
+  let sourceFinished = false
+  let index = 0
+  const values = [
+    async () => {
+      await delay(500)
+      throw new Error('Urk!')
+    },
+    async () => {
+      await delay(100)
+      return 'hello'
+    },
+    async () => {
+      await delay(200)
+      return 'hello'
+    },
+    async () => {
+      await delay(300)
+      return 'hello'
+    },
+    async () => {
+      await delay(400)
+      return 'hello'
+    },
+    async () => {
+      await delay(500)
+      return 'world'
+    },
+    async () => {
+      await delay(600)
+      return 'world'
+    }
+  ]
+
+  const source = {
+    [Symbol.asyncIterator]: () => source,
+    async next () {
+      const value = values[index]
+      index++
+
+      return Promise.resolve({
+        done: index === values.length,
+        value
+      })
+    },
+    return () {
+      sourceFinished = true
+    }
+  }
+
+  try {
+    await all(parallel(source, { concurrency: 2, ordered: true }))
+  } catch {
+    t.truthy(sourceFinished)
+  }
+})
+
+test('Should work without concurrency parameter', async (t) => {
   const input = [
     createFn(200, 1),
     createFn(100, 2)
   ]
 
-  const res = await all(parallel(input))
+  const res = await all(parallel(input, { ordered: true }))
 
   t.deepEqual(res, [1, 2])
 })
@@ -98,8 +190,8 @@ test('Should batch up entries with negative batch size', async (t) => {
     createFn(200, 1),
     createFn(100, 2)
   ]
-  const batchSize = -1
-  const res = await all(parallel(input, batchSize))
+  const concurrency = -1
+  const res = await all(parallel(input, { concurrency, ordered: true }))
 
   t.deepEqual(res, [1, 2])
 })
@@ -109,8 +201,8 @@ test('Should batch up entries with zero batch size', async (t) => {
     createFn(200, 1),
     createFn(100, 2)
   ]
-  const batchSize = 0
-  const res = await all(parallel(input, batchSize))
+  const concurrency = 0
+  const res = await all(parallel(input, { concurrency, ordered: true }))
 
   t.deepEqual(res, [1, 2])
 })
@@ -120,8 +212,8 @@ test('Should batch up entries with string batch size', async (t) => {
     createFn(200, 1),
     createFn(100, 2)
   ]
-  const batchSize = '2'
-  const res = await all(parallel(input, batchSize))
+  const concurrency = '2'
+  const res = await all(parallel(input, { concurrency, ordered: true }))
 
   t.deepEqual(res, [1, 2])
 })
@@ -131,8 +223,8 @@ test('Should batch up entries with non-integer batch size', async (t) => {
     createFn(200, 1),
     createFn(100, 2)
   ]
-  const batchSize = 2.5
-  const res = await all(parallel(input, batchSize))
+  const concurrency = 2.5
+  const res = await all(parallel(input, { concurrency, ordered: true }))
 
   t.deepEqual(res, [1, 2])
 })
@@ -150,8 +242,15 @@ test('Should allow returning errors', async (t) => {
       return new Error('derp')
     }
   ]
-  const batchSize = 2
-  const res = await all(parallel(input, batchSize))
+  const concurrency = 2
+  const res = await all(parallel(input, { concurrency, ordered: true }))
 
   t.deepEqual(res, [new Error('herp'), new Error('derp')])
+})
+
+test('Should work with empty source', async (t) => {
+  const input = []
+  const res = await all(parallel(input))
+
+  t.deepEqual(res, [])
 })
