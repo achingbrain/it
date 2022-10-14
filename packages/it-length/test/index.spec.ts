d2908f888331d0093cf0a0a159c3c667ad755dc6
achingbrain <alex@achingbrain.net>
2022-10-14 08:06:01 +0100

    feat!: convert to typescript
    Converts all modules to typescript.
    
    BREAKING CHANGE: all modules are now published as ESM-only

---
+++ packages/it-length/test/index.spec.ts
@@ -0,0 +1,10 @@
+const length = require('../dist')
+const test = require('ava')
+
+test('Should count the items in an async iterator', async (t) => {
+  const values = [0, 1, 2, 3, 4]
+
+  const res = await length(values)
+
+  t.is(res, 5)
+})
