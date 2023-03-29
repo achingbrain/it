c34a0eb84ce73d77210639b9182b9ed4536730a6
achingbrain <alex@achingbrain.net>
2023-03-29 17:06:48 +0100

    chore: make it-merge return sync

--- packages/it-merge/src/index.ts
+++ packages/it-merge/src/index.ts
@@ -1,31 +1,58 @@
 import { pushable } from 'it-pushable'
 
+function isAsyncIterable <T> (thing: any): thing is AsyncIterable<T> {
+  return thing[Symbol.asyncIterator] != null
+}
+
 /**
  * Treat one or more iterables as a single iterable.
  *
  * Nb. sources are iterated over in parallel so the
  * order of emitted items is not guaranteed.
  */
-export default async function * merge <T> (...sources: Array<AsyncIterable<T> | Iterable<T>>): AsyncGenerator<T, void, undefined> {
-  const output = pushable<T>({
-    objectMode: true
-  })
+function merge <T> (...sources: Array<Iterable<T>>): Generator<T, void, undefined>
+function merge <T> (...sources: Array<AsyncIterable<T> | Iterable<T>>): AsyncGenerator<T, void, undefined>
+function merge <T> (...sources: Array<AsyncIterable<T> | Iterable<T>>): AsyncGenerator<T, void, undefined> | Generator<T, void, undefined> {
+  const syncSources: Array<Iterable<T>> = []
 
-  void Promise.resolve().then(async () => {
-    try {
-      await Promise.all(
-        sources.map(async (source) => {
-          for await (const item of source) {
-            output.push(item)
-          }
-        })
-      )
-
-      output.end()
-    } catch (err: any) {
-      output.end(err)
+  for (const source of sources) {
+    if (!isAsyncIterable(source)) {
+      syncSources.push(source)
     }
-  })
+  }
 
-  yield * output
+  if (syncSources.length === sources.length) {
+    // all sources are synchronous
+    return (function * () {
+      for (const source of syncSources) {
+        yield * source
+      }
+    })()
+  }
+
+  return (async function * () {
+    const output = pushable<T>({
+      objectMode: true
+    })
+
+    void Promise.resolve().then(async () => {
+      try {
+        await Promise.all(
+          sources.map(async (source) => {
+            for await (const item of source) {
+              output.push(item)
+            }
+          })
+        )
+
+        output.end()
+      } catch (err: any) {
+        output.end(err)
+      }
+    })
+
+    yield * output
+  })()
 }
+
+export default merge
--- packages/it-merge/test/index.spec.ts
+++ packages/it-merge/test/index.spec.ts
@@ -7,7 +7,36 @@ describe('it-merge', () => {
     const values1 = [0, 1, 2, 3, 4]
     const values2 = [5, 6, 7, 8, 9]
 
-    const res = await all(merge(values1, values2))
+    const gen = merge(values1, values2)
+    expect(gen[Symbol.iterator]).to.be.ok()
+
+    const res = all(gen)
+
+    expect(res.sort((a, b) => {
+      if (a < b) {
+        return -1
+      }
+
+      if (a > b) {
+        return 1
+      }
+
+      return 0
+    })).to.deep.equal([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+  })
+
+  it('should merge multiple async iterators', async () => {
+    const values1 = async function * (): AsyncGenerator<number, void, undefined> {
+      yield * [0, 1, 2, 3, 4]
+    }
+    const values2 = async function * (): AsyncGenerator<number, void, undefined> {
+      yield * [5, 6, 7, 8, 9]
+    }
+
+    const gen = merge(values1(), values2())
+    expect(gen[Symbol.asyncIterator]).to.be.ok()
+
+    const res = await all(gen)
 
     expect(res.sort((a, b) => {
       if (a < b) {
