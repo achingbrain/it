fa75c453cc207c699f539713554fc63200d47c4c
achingbrain <alex@achingbrain.net>
2023-03-31 10:00:09 +0100

    fix!: return iterators from synchronous sources
    Applies the same changes from #55 to it-filter
    
    BREAKING CHANGE: if you pass a synchronous iterator to a function it will return a synchronous generator in response

--- packages/it-filter/test/index.spec.ts
+++ packages/it-filter/test/index.spec.ts
@@ -2,20 +2,47 @@ import { expect } from 'aegir/chai'
 import all from 'it-all'
 import filter from '../src/index.js'
 
+function * values (): Generator<number, void, undefined> {
+  yield * [0, 1, 2, 3, 4]
+}
+
+async function * asyncValues (): AsyncGenerator<number, void, undefined> {
+  yield * values()
+}
+
 describe('it-filter', () => {
   it('should filter all values greater than 2', async () => {
-    const values = [0, 1, 2, 3, 4]
+    const res = all(filter(values(), val => val > 2))
 
-    const res = await all(filter(values, val => val > 2))
+    expect(res[Symbol.iterator]).to.be.ok()
+    expect(res).to.deep.equal([3, 4])
+  })
 
+  it('should filter all values greater than 2 with a promise', () => {
+    const res = all(filter(values(), val => val > 2))
+
+    expect(res[Symbol.iterator]).to.be.ok()
     expect(res).to.deep.equal([3, 4])
   })
 
   it('should filter all values greater than 2 with a promise', async () => {
-    const values = [0, 1, 2, 3, 4]
+    const res = filter(values(), async val => val > 2)
 
-    const res = await all(filter(values, async val => val > 2))
+    expect(res[Symbol.asyncIterator]).to.be.ok()
+    await expect(all(res)).to.eventually.deep.equal([3, 4])
+  })
 
-    expect(res).to.deep.equal([3, 4])
+  it('should filter all async values greater than 2', async () => {
+    const res = filter(asyncValues(), val => val > 2)
+
+    expect(res[Symbol.asyncIterator]).to.be.ok()
+    await expect(all(res)).to.eventually.deep.equal([3, 4])
+  })
+
+  it('should filter all async values greater than 2 with a promise', async () => {
+    const res = filter(asyncValues(), async val => val > 2)
+
+    expect(res[Symbol.asyncIterator]).to.be.ok()
+    await expect(all(res)).to.eventually.deep.equal([3, 4])
   })
 })
