8408717ee305c48e3dbdc41fdb3195b07ef2612b
achingbrain <alex@achingbrain.net>
2023-11-12 13:41:01 +0100

    chore: add byte stream benchmark
    Useful for low-level performance testing.

---
+++ packages/it-byte-stream/benchmark/stream.js
@@ -0,0 +1,36 @@
+import { duplexPair } from 'it-pair/duplex'
+import { Uint8ArrayList } from 'uint8arraylist'
+import { byteStream } from '../dist/src/index.js'
+
+const DATA_LENGTH = 1024 * 1024 * 1024
+const CHUNK_SIZE = 256 * 1024
+const ITERATIONS = 10
+
+const results = []
+
+for (let i = 0; i < ITERATIONS; i++) {
+  const duplex = duplexPair()
+  const inputStream = byteStream(duplex[0])
+  const outputStream = byteStream(duplex[1])
+  let read = 0
+
+  const start = Date.now()
+
+  while (read < DATA_LENGTH) {
+    await inputStream.writeV(
+      new Array(5).fill(0).map(() => new Uint8Array(CHUNK_SIZE))
+    )
+    const buf = await outputStream.read()
+
+    read += buf.byteLength
+  }
+
+  const finish = Date.now() - start
+  results.push(finish)
+}
+
+const megs = DATA_LENGTH / (1024 * 1024)
+const secs = (results.reduce((acc, curr) => acc + curr, 0) / results.length) / 1000
+
+// eslint-disable-next-line no-console
+console.info((megs / secs).toFixed(2), 'MB/s')
