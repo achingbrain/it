f28b389f2eb1f963437e10ec6b1790b11b4f8cb1
achingbrain <alex@achingbrain.net>
2026-05-05 14:21:58 +0300

    chore: tests

--- packages/it-glob/test/index.spec.ts
+++ packages/it-glob/test/index.spec.ts
@@ -64,20 +64,20 @@ describe('it-glob', () => {
   })
 
   it('should match directories', async () => {
-    const files = await all(glob(path.resolve(dir, '..', '..'), 'dist/*', {
+    const files = await all(glob(path.resolve(dir, '..'), '*/**', {
       onlyFiles: false
     }))
 
-    expect(files.includes('dist/src')).to.be.true()
+    expect(files.includes('src')).to.be.true()
   })
 
   it('should skip directories', async () => {
-    const files = await all(glob(path.resolve(dir, '..', '..'), 'dist/**/*', {
+    const files = await all(glob(path.resolve(dir, '..'), '*/**', {
       onlyFiles: true,
       dot: true
     }))
 
-    expect(files.includes('dist/src')).to.be.false()
-    expect(files.includes('dist/src/index.js')).to.be.true()
+    expect(files.includes('src')).to.be.false()
+    expect(files.includes('src/index.ts')).to.be.true()
   })
 })
